4d60fdd5b27c2e88cd623a73b5e8103eb485143d
Sharon Nrangi <sharonnarangi@gmail.com>
2024-10-02 10:52:49 +0300

    DOM manipulation

---
+++ index.js
@@ -0,0 +1,74 @@
+/* function displayData(data) {
+  const container = document.getElementById("displayData"); //attaining land
+
+  const itemDiv = document.createElement("div"); //used to create an element
+  itemDiv.className = "item"; */ //used to add a class to the element
+  /* itemDiv.classList.add('item')..used to add many classes or a list of classes that eenable looping */
+ /*  itemDiv.innerHTML = `
+
+    <h1>${data.title} </h1>
+    <h6>${data.message}</h6>
+    `; */
+ /*  container.appendChild(itemDiv); //joining the item div to the html container
+}
+
+const myData = {
+  title: "hello sharon",
+  message: "hello world welcome to programming",
+};
+displayData(myData); */
+
+//fetch api >>GET(RETRIVE DATA) ,PUT(UPDATE),POST(SENDING DATA ), DELETE(TO REMOVE),
+//USES HTTPS METHODS TO ACCESS INFORMATION
+//it has two paramaeters ((url),{object/position})
+//a fetch operation will always  conclude either  a true or a false
+//it uses asynchronous code to run get and execute
+//it is a promise based operation
+//will check on promises
+
+/* fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  .then((response) => response.json())
+   .then(data=> data.json)
+     .catch((error) => console.log(error)); */
+
+  /*    try to create a div that will contain the pokemon 
+     //container 
+     create an image tag with empty src atribute 
+     fecth information from the  pokemon api using the fetch method and the .then 
+     in js \..create a function that when called creates the duv and displays the pokemon//input type search that when a user searches for a pokemon it displays the image  */
+
+
+  //fetching data asynchronously
+ // Function to fetch Pokémon data
+ async function getPokemon() {
+    // Getting the search input element and its value
+    const searchInput = document.getElementById("searchelement");
+    const searchValue = searchInput.value.toLowerCase(); // Convert the input to lowercase
+  
+    try {
+      // Fetching data from the Pokémon API using the user input
+      const response = await fetch(https://pokeapi.co/api/v2/pokemon/${searchValue});
+  
+      // Checking if the resource is available
+      if (!response.ok) {
+        throw new Error("The Pokémon could not be found.");
+      }
+  
+      // Parsing the response data to JSON
+      const data = await response.json();
+  
+      // Logging the data to the console
+      console.log("This is your data:", data);
+  
+      // Displaying the Pokémon image
+      const pokemonImage = data.sprites.front_default; // Getting the Pokémon image from the API response
+      document.querySelector("img").src = pokemonImage; // Setting the image source in the HTML
+  
+    } catch (error) {
+      // Catching and logging any errors that occur
+      console.log("Error:", error.message);
+    }
+  }
+  
+  // Adding an event listener to the search button
+  document.getElementById("searchme").addEventListener("click", getPokemon);
